ade851e7495b7c36a76a35e036c5c315929595ea
anton-mladenov <anton.g.mladenov@gmail.com>
2018-11-25 22:20:38 +0100

    stylizing Back Button

--- client/src/components/BackButton.js
+++ client/src/components/BackButton.js
@@ -4,26 +4,48 @@ import { View, Button, TextInput } from "react-native"
 import { withNavigation } from "react-navigation"
 import { getAllCategories } from "../actions/categoriesActions"
 import { getAllExpenses } from "../actions/expensesActions"
+import { Appbar } from 'react-native-paper';
+
+class MyComponent extends React.Component {
+  _goBack = () => console.log('Went back');
+
+  _onSearch = () => console.log('Searching');
+
+  _onMore = () => console.log('Shown more');
+
+  render() {
+    return (
+      <Appbar.Header>
+        <Appbar.BackAction
+          onPress={this._goBack}
+        />
+      </Appbar.Header>
+    );
+  }
+}
+
 
 class BackButton extends Component {
 
-    render() {
-
+    _goBack = () => {
         const { category, categoryId } = this.props
         const catId = parseInt(categoryId, 10)
+        category === "AllCategories" ? this.props.getAllCategories() : console.log("looking for expenses, not categories")
+        category === "AllExpenses" ? this.props.getAllExpenses(catId) : console.log("looking for categories, not expenses")
+        this.props.navigation.goBack()
+    }
+
+    render() {
+
         
         return (
             <View>
-                <Button
-                    title="Back"
-                    onPress={() =>
-                        {
-                            category === "AllCategories" ? this.props.getAllCategories() : console.log("looking for expenses, not categories")
-                            category === "AllExpenses" ? this.props.getAllExpenses(catId) : console.log("looking for categories, not expenses")
-                            this.props.navigation.goBack()
-                        }
-                    }
-                />
+                <Appbar.Header
+                style={{backgroundColor: "black"}}>
+                    <Appbar.BackAction
+                    onPress={this._goBack}
+                    />
+                </Appbar.Header>
             </View>
         )
     }
