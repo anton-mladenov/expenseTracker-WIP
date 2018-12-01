2e7617b0ec5f4a7a1c74a84486bd3c9fef8951f3
anton-mladenov <anton.g.mladenov@gmail.com>
2018-12-01 14:02:19 +0100

    add styling to Expense Form

--- client/src/components/expenses/ExpensesForm.js
+++ client/src/components/expenses/ExpensesForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { ScrollView, Button, TextInput } from "react-native"
+import { ScrollView, TextInput, StyleSheet } from "react-native"
+import { Button, Title } from 'react-native-paper';
 
 export default class ExpensesForm extends Component {
 
@@ -20,20 +21,46 @@ export default class ExpensesForm extends Component {
                     placeholder="so... what did you buy?"
                     onChangeText={ (name) => this.setState({ name: name }) }
                     value={ this.state.name !== undefined ? this.state.name : initialValues.name }
+                    placeholderTextColor="white"
                 />
 
                 <TextInput
                     placeholder="...and how much does that cost?"
                     onChangeText={ (amount) => this.setState({ amount: amount }) }
                     value={ this.state.amount !== undefined ? this.state.amount : initialValues.amount }
+                    placeholderTextColor="white"
                 />
 
                 <Button
-                    title="Submit"
+                    mode="contained"
                     onPress={ this.handleSubmit }
-                />
+                    style={{ flex:1,
+                        flexDirection:'row',
+                        alignItems:'center',
+                        justifyContent:'center',
+                        marginHorizontal: 60,
+                        marginVertical: 5,
+                        backgroundColor: "#FF951C", 
+                        color: styles.buttonTextColor.color,
+                        borderBottomWidth: 0.3,
+                        borderRightWidth: 0.3,
+                        borderColor: "white"
+                    }}
+                > Submit </Button>
 
             </ScrollView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: "#0B3954",
+    },
+    buttonBackground: {
+        backgroundColor: "#00D0E5"
+    },
+    buttonTextColor: {
+        color: "white"
+    }
+})
