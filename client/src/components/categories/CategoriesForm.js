623606247a9e59188174e90be5b1c4d57fb3b8c0
anton-mladenov <anton.g.mladenov@gmail.com>
2018-12-01 15:07:07 +0100

    update styling of Category Form

--- client/src/components/categories/CategoriesForm.js
+++ client/src/components/categories/CategoriesForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { ScrollView, TextInput, StyleSheet } from "react-native"
+import { ScrollView, TextInput, StyleSheet, View } from "react-native"
 import { Button, Title } from 'react-native-paper'
 import { withNavigation } from "react-navigation"
 
@@ -17,30 +17,46 @@ class CategoriesForm extends Component {
 
         return (
             <ScrollView>
-
-                <TextInput
-                    placeholder="Give A Name To Your New Category."
-                    onChangeText={ (name) => this.setState({ name: name }) }
-                    value={ this.state.name !== undefined ? this.state.name : initialValues.name }
-                    placeholderTextColor="white"
-                />
-
-                <Button
-                    mode="contained"
-                    onPress={ this.handleSubmit }
+                
+                <View 
                     style={{ flex:1,
-                        flexDirection:'row',
+                        flexDirection:'column',
                         alignItems:'center',
                         justifyContent:'center',
-                        marginHorizontal: 60,
-                        marginVertical: 5,
-                        backgroundColor: "#FF951C", 
-                        color: styles.buttonTextColor.color,
-                        borderBottomWidth: 0.3,
-                        borderRightWidth: 0.3,
-                        borderColor: "white"
-                    }}
-                > Submit </Button>
+                        marginBottom: 40,
+                        marginTop: "30%",
+                    }} 
+                    >
+                    <TextInput
+                        placeholder="Give A Name To Your New Category."
+                        onChangeText={ (name) => this.setState({ name: name }) }
+                        value={ this.state.name !== undefined ? this.state.name : initialValues.name }
+                        placeholderTextColor="white"
+                        style={{ 
+                            textAlign: "center",
+                            color: "white",
+                            marginBottom: 40,
+                        }}
+                    />
+
+                    <Button
+                        mode="contained"
+                        onPress={ this.handleSubmit }
+                        style={{ flex:1,
+                            flexDirection:'row',
+                            alignItems:'center',
+                            justifyContent:'center',
+                            marginHorizontal: 60,
+                            marginVertical: 5,
+                            backgroundColor: "#FF951C", 
+                            color: styles.buttonTextColor.color,
+                            borderBottomWidth: 0.3,
+                            borderRightWidth: 0.3,
+                            borderColor: "white"
+                        }}
+                    > Submit </Button>
+
+                </View>
 
             </ScrollView>
         )
