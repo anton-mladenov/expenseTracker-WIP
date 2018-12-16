73fe7daacda921a8b927f4421a20d4bee17d078d
anton-mladenov <anton.g.mladenov@gmail.com>
2018-12-16 22:19:58 +0100

    add styling to SignUp screen

--- client/src/components/account/SignUp.js
+++ client/src/components/account/SignUp.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import { ScrollView, Button, StyleSheet } from "react-native"
+import { ScrollView, StyleSheet, View } from "react-native"
 import { connect } from "react-redux"
 import SignUpForm from "./SignUpForm"
 import { newSignUp } from "../../actions/usersActions"
-import { Title } from 'react-native-paper'
+import { Title, Button, Text } from 'react-native-paper'
 
 class SignUp extends Component {
 
@@ -37,19 +37,39 @@ class SignUp extends Component {
 
                 {
                     signUpSuccess &&
-                    <Text
-                    style={{ color: styles.buttonTextColor.color, textAlign: "center", marginTop: 70, marginBottom: 30 }}
-                    > 
-                    Awesome! You have an account now. You now can log in. 
-                    </Text>
+                    <View>
+                        <Text
+                        style={{ color: styles.buttonTextColor.color, textAlign: "center", marginTop: 70, marginBottom: 30 }}
+                        > 
+                        Awesome! You have an account now. 
+                        </Text>
+                        <Text
+                        style={{ color: styles.buttonTextColor.color, textAlign: "center", marginBottom: 30 }}
+                        > 
+                        You now can log in. 
+                        </Text>
+                    </View>
                 }
 
                 {
                     signUpSuccess &&
                     <Button
-                        title="Go To Login Page"
-                        onPress={ () => this.props.navigation.navigate("WelcomeScreen") }
-                    />
+                    mode="contained"
+                    onPress={ () => this.props.navigation.navigate("WelcomeScreen") }
+                    style={{ 
+                        flex:1,
+                        flexDirection:'row',
+                        alignItems:'center',
+                        justifyContent:'center',
+                        marginHorizontal: 60,
+                        marginVertical: 40,
+                        backgroundColor: "#FF951C", 
+                        color: styles.buttonTextColor.color,
+                        borderBottomWidth: 0.3,
+                        borderRightWidth: 0.3,
+                        borderColor: "white",
+                    }}
+                > Go To Login Page </Button>
                 }
 
             </ScrollView>
